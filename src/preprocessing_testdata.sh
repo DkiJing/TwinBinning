7945cdf34d8accede7222ce7466300812989e0f2
DkiJing <11612023@mail.sustc.edu.cn>
2021-07-24 17:03:29 +0800

    Update preprocessing testdata

--- src/preprocessing_testdata.sh
+++ src/preprocessing_testdata.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-src="/home/jli347/src"
-species="/home/jli347/data/test_dataset/selected_species"
-contigs="/home/jli347/data/test_dataset/output_contigs"
-threemer="/home/jli347/data/test_dataset/3-mer/kmer.csv"
-fourmer="/home/jli347/data/test_dataset/4-mer/test.csv"
-feature="/home/jli347/data/test_dataset/3-mer/test.csv"
-cov="/home/jli347/data/test_dataset/3-mer/abundance.csv"
-covfreq="/home/jli347/data/test_dataset/3-mer/abundance_profile.csv"
-covmyout="/home/jli347/data/test_dataset/cov_diff_5/myout"
-reads="/home/jli347/data/test_dataset/cov_diff_5/reads"
+parentdir="$(dirname `pwd`)"
+src=$parentdir/src
+species=$parentdir/data/test_dataset/selected_species
+contigs=$parentdir/data/test_dataset/output_contigs
+threemer=$parentdir/data/test_dataset/3-mer/kmer.csv
+fourmer=$parentdir/data/test_dataset/4-mer/test.csv
+feature=$parentdir/data/test_dataset/3-mer/test.csv
+covfreq=$parentdir/data/test_dataset/3-mer/abundance_profile.csv
+covmyout=$parentdir/test_dataset/coverage/myout
+reads=$parentdir/data/test_dataset/coverage/reads
+contig_len=4000
 
 echo "Cut contigs..."
 for i in `ls $species`
 do
-  python contig_generation.py -s $species/$i/`ls "$species/$i"` -b 4000 -o $contigs/$i.fa
+  python contig_generation.py -s $species/$i/`ls "$species/$i"` -b $contig_len -o $contigs/$i.fa
 done
 echo "Kmer extraction..."
 label=0
@@ -24,16 +25,15 @@ do
   label=$((label+1))
 done
 echo "Generate abundance profile..."
-cat $contigs/*.fa > $covmyout/Bacteroides_pairs.fa 
+cat $contigs/*.fa > $covmyout/combined_pairs.fa 
 cd $covmyout
-bowtie2-build Bacteroides_pairs.fa myout.idx 1>myout.sam.bowtie2build.out 2>myout.sam.bowtie2build.err
-bowtie2 -p 4 -x myout.idx -U $reads/Bacteroides_all_reads.fq -S myout.sam 1>myout.sam.bowtie2.out 2>myout.sam.bowtie2.err
+bowtie2-build combined_pairs.fa myout.idx 1>myout.sam.bowtie2build.out 2>myout.sam.bowtie2build.err
+bowtie2 -p 4 -x myout.idx -U $reads/combined_all_reads.fq -S myout.sam 1>myout.sam.bowtie2.out 2>myout.sam.bowtie2.err
 samtools view -bS myout.sam > myout.bam
 samtools sort myout.bam -o myout.sorted.bam
 samtools depth myout.sorted.bam > output.txt
 average=`awk '{sum+=$3} END{print sum/NR}' output.txt`
 echo "Average depth is $average"
 cd $src
-python abundance_vector.py -l 4000 -i $covmyout/output.txt -o $cov
-python abundance_profile.py -i $cov -o $covfreq -m $average
+python abundance_profile.py -l $contig_len -i $covmyout/output.txt -o $covfreq -m $average
 python feature_combined_vector.py -k $threemer -a $covfreq -o $feature 
