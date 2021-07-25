7bc781e0b3050103fe4d1604571b044e09af92d0
DkiJing <11612023@mail.sustc.edu.cn>
2021-07-25 15:15:37 +0800

    add trainer script

---
+++ src/trainer.py
@@ -0,0 +1,186 @@
+import torch
+import numpy as np
+
+
+def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval, metrics=[],
+        start_epoch=0):
+    """
+    Loaders, model, loss function and metrics should work together for a given task,
+    i.e. The model should be able to process data output of loaders,
+    loss function should process target output of loaders and outputs from the model
+
+    Examples: Classification: batch loader, classification model, NLL loss, accuracy metric
+    Siamese network: Siamese loader, siamese model, contrastive loss
+    Online triplet learning: batch loader, embedding model, online triplet loss
+    """
+    loss_values_train = []
+    loss_values_val = []
+    accuracy_values_train = []
+    accuracy_values_val = []
+    
+    for epoch in range(0, start_epoch):
+        scheduler.step()
+
+    for epoch in range(start_epoch, n_epochs):
+        scheduler.step()
+
+        # Train stage
+        train_loss, train_metrics = train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, metrics)
+
+        message = 'Epoch: {}/{}. Train set: Average loss: {:.4f}'.format(epoch + 1, n_epochs, train_loss)
+        
+        loss_values_train.append(train_loss)
+
+        for metric in train_metrics:
+            message += '\t{}: {}'.format(metric.name(), metric.value())
+            accuracy_values_train.append(metric.value())
+
+        val_loss, val_metrics = test_epoch(val_loader, model, loss_fn, cuda, metrics)
+        val_loss /= len(val_loader)
+
+        message += '\nEpoch: {}/{}. Validation set: Average loss: {:.4f}'.format(epoch + 1, n_epochs,
+                                                                                 val_loss)
+        
+        loss_values_val.append(val_loss)
+
+        for metric in val_metrics:
+            message += '\t{}: {}'.format(metric.name(), metric.value())
+            accuracy_values_val.append(metric.value())
+
+        print(message)
+        
+    return loss_values_train, loss_values_val, accuracy_values_train, accuracy_values_val
+
+
+def train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, metrics):
+    for metric in metrics:
+        metric.reset()
+    
+    model.train()
+    losses = []
+    total_loss = 0
+    
+    for batch_idx, (data, target) in enumerate(train_loader):
+        target = target if len(target) > 0 else None
+        if not type(data) in (tuple, list):
+            data = (data,)
+        if cuda:
+            data = tuple(d.cuda() for d in data)
+            if target is not None:
+                target = target.cuda()
+
+        optimizer.zero_grad()
+        outputs = model(*data)
+
+        if type(outputs) not in (tuple, list):
+            outputs = (outputs,)
+
+        loss_inputs = outputs
+        if target is not None:
+            target = (target,)
+            loss_inputs += target
+
+        loss_outputs = loss_fn(*loss_inputs)
+        loss = loss_outputs[0] if type(loss_outputs) in (tuple, list) else loss_outputs
+        losses.append(loss.item())
+        total_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+
+        for metric in metrics:
+            metric(outputs, target, loss_outputs)
+
+        if batch_idx % log_interval == 0:
+            message = 'Train: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                batch_idx * len(data[0]), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), np.mean(losses))
+            for metric in metrics:
+                message += '\t{}: {}'.format(metric.name(), metric.value())
+
+            print(message)
+            losses = []
+
+    total_loss /= (batch_idx + 1)
+    return total_loss, metrics
+
+
+def test_epoch(val_loader, model, loss_fn, cuda, metrics):
+    with torch.no_grad():
+        for metric in metrics:
+            metric.reset()
+        model.eval()
+        val_loss = 0
+        for batch_idx, (data, target) in enumerate(val_loader):
+            target = target if len(target) > 0 else None
+            if not type(data) in (tuple, list):
+                data = (data,)
+            if cuda:
+                data = tuple(d.cuda() for d in data)
+                if target is not None:
+                    target = target.cuda()
+
+            outputs = model(*data)
+
+            if type(outputs) not in (tuple, list):
+                outputs = (outputs,)
+            loss_inputs = outputs
+            if target is not None:
+                target = (target,)
+                loss_inputs += target
+
+            loss_outputs = loss_fn(*loss_inputs)
+            loss = loss_outputs[0] if type(loss_outputs) in (tuple, list) else loss_outputs
+            val_loss += loss.item()
+
+            for metric in metrics:
+                metric(outputs, target, loss_outputs)
+
+    return val_loss, metrics
+
+def normalize(vector):
+    scale = 1.0 / (vector.max(dim=0, keepdim=True)[0] - vector.min(dim=0, keepdim=True)[0]) 
+    vector.mul_(scale).sub_(vector.min(dim=0, keepdim=True)[0])
+    return vector
+
+def mysigmoid(x):
+    return 1 + (-1 / (1 + np.exp(-x)))
+
+def prediction(val_loader, model, loss_fn, cuda, metrics):
+    threshold_dist = 0.1
+    pred_list = []
+    with torch.no_grad():
+        for metric in metrics:
+            metric.reset()
+        model.eval()
+        val_loss = 0
+        for batch_idx, (data, target) in enumerate(val_loader):
+            target = target if len(target) > 0 else None
+            if not type(data) in (tuple, list):
+                data = (data,)
+            if cuda:
+                data = tuple(d.cuda() for d in data)
+                if target is not None:
+                    target = target.cuda()
+
+            outputs = model(*data)
+
+            if type(outputs) not in (tuple, list):
+                outputs = (outputs,)
+            loss_inputs = outputs
+            if target is not None:
+                target = (target,)
+                loss_inputs += target
+
+            loss_outputs = loss_fn(*loss_inputs)
+            loss = loss_outputs[0] if type(loss_outputs) in (tuple, list) else loss_outputs
+            val_loss += loss.item()
+
+            for metric in metrics:
+                metric(outputs, target, loss_outputs)
+
+            distances = (outputs[0].data - outputs[1].data).pow(2).sum(1)  # squared distances
+            normalized_distances = normalize(distances).reshape((-1, 1))
+            pred = np.round(mysigmoid(normalized_distances - threshold_dist)).type(torch.LongTensor)
+            pred_list.append(pred)
+
+    return pred_list, metrics
